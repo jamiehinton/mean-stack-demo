ca358e5f614b5ba2e47e8da9b0d67f218a09b6ef
jhinton <jhinton@technophobia.com>
2013-12-24 09:16:52 +0000

    finished

---
+++ public/js/services/players.js
@@ -0,0 +1,10 @@
+'use strict'
+window.angular.module('ngff.services.players', [])
+    .factory('Players', ['$resource', function($resource){
+        return $resource(
+          'players/:playerId',
+            {
+                playerId: '@_id'
+            }
+        );
+    }]);
